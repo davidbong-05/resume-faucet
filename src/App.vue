6c04247a8b84588d08764fdfa3c1e3004eea353d
davidbong-05 <davidbong05@gmail.com>
2023-05-05 14:15:59 +0800

    style: added tech stack

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,77 @@
 <template>
-	<main>
-		<div class="dark:bg-slate-200 rounded-lg shadow-lg p-8">
-			<div class="flex justify-center">
-				<img
-					class="w-32 h-32 rounded-full object-cover mr-4"
-					src="@/assets/profile_pic.jpg"
-					alt="profile_pic"
-				/>
-				<div>
-					<h1 class="text-xl font-bold mb-2">David Bong Xia Shing</h1>
-					<p class="text-gray-600">Future Software Developer</p>
-					<p class="mt-2 text-gray-600">
-						I am currently in my <b>final semester</b> of my
-						<b>
-							Bachelor of Computer Science (Software Engineering) student with
-							Honours
-						</b>
-						from <i>Universiti Malaysia Sarawak</i>. I am looking for a
-						<b>fulltime software developer</b> role to begin the next chapter of
-						my life. 
-					</p>
-					<p class="text-gray-600"><b>Expected Graduation: July 2023</b></p>
-					<p class="mt-2 text-gray-600">
-						<b>Contact:</b> davidbong05@gmail.com
-					</p>
-					<div class="mt-4 flex">
-						<button
-							onclick="window.location.href='https://github.com/davidbong-05'"
-							class="flex me-2 rounded-md dark:bg-slate-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+	<main class="grid justify-items-center">
+		<div class="flex justify-center md:w-8/12 dark:bg-slate-300  rounded-lg shadow-lg p-8">
+			<img
+				class="w-32 h-32 rounded-full object-cover mr-4"
+				src="@/assets/profile_pic.jpg"
+				alt="profile_pic"
+			/>
+			<div>
+				<h1 class="text-xl font-bold mb-2">David Bong Xia Shing</h1>
+				<p class="text-gray-600">Future Software Developer</p>
+				<p class="mt-2 text-gray-600">
+					I am currently in my <b>final semester</b> of my
+					<b>
+						Bachelor of Computer Science (Software Engineering) student with
+						Honours
+					</b>
+					from <i>Universiti Malaysia Sarawak</i>. I am looking for a
+					<b>fulltime software developer</b> role to begin the next chapter of
+					my life. 
+				</p>
+				<p class="text-gray-600 mt-3 font-semibold">Expected Graduation: July 2023</p>
+				<p class="text-gray-600">
+					<h4 class="font-bold mt-3">My Tech Stack</h4>
+					<ul class="list-disc">
+						<li class="ms-6"><span class="font-semibold text-gray-600">Languages:</span> JavaScript, PHP, Solidity, C/C++</li> 
+						<li class="ms-6"><span class="font-semibold text-gray-600">Frontend:</span> Vue.js, HTML, CSS, Bootstrap, Tailwind CSS</li>
+						<li class="ms-6"><span class="font-semibold text-gray-600">Backend:</span> Laravel, Node.js, Express.js, MongoDB, MySQL</li>
+						<li class="ms-6"><span class="font-semibold text-gray-600">Others:</span> Git, GitHub, Heroku, Netlify</li>
+					</ul>
+				</p>
+				<p class="mt-2 text-gray-600">
+					<b>Contact:</b> davidbong05@gmail.com
+				</p>
+				<div class="mt-4 flex">
+					<button
+						onclick="window.location.href='https://github.com/davidbong-05'"
+						class="flex me-2 rounded-md dark:bg-slate-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					>
+						<svg
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="currentColor"
+							class="h-5 w-5 me-2"
 						>
-							<svg
-								viewBox="0 0 24 24"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="currentColor"
-								class="h-5 w-5 me-2"
-							>
-								<path
-									d="M12 .298C5.37.298 0 5.675 0 12.306c0 5.406 3.438 9.98 8.205 11.583.6.112.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.39-1.332-1.76-1.332-1.76-1.088-.743.083-.727.083-.727 1.203.085 1.838 1.237 1.838 1.237 1.07 1.836 2.81 1.304 3.495.997.108-.776.418-1.305.76-1.605-2.665-.306-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.307-.54-1.53.105-3.182 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.4 3-.405 1.02.005 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.652.24 2.875.12 3.182.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.62-5.475 5.92.42.36.81 1.096.81 2.22 0 1.607-.015 2.897-.015 3.287 0 .315.21.69.825.57C20.565 22.28 24 17.707 24 12.305c0-6.63-5.373-12.008-12-12.008z"
-								/>
-							</svg>
-							GitHub
-						</button>
-						<button
-							onclick="window.location.href='https://www.linkedin.com/in/davidbong05/'"
-							class="flex rounded-md dark:bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+							<path
+								d="M12 .298C5.37.298 0 5.675 0 12.306c0 5.406 3.438 9.98 8.205 11.583.6.112.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61-.546-1.39-1.332-1.76-1.332-1.76-1.088-.743.083-.727.083-.727 1.203.085 1.838 1.237 1.838 1.237 1.07 1.836 2.81 1.304 3.495.997.108-.776.418-1.305.76-1.605-2.665-.306-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.307-.54-1.53.105-3.182 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.4 3-.405 1.02.005 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.652.24 2.875.12 3.182.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.62-5.475 5.92.42.36.81 1.096.81 2.22 0 1.607-.015 2.897-.015 3.287 0 .315.21.69.825.57C20.565 22.28 24 17.707 24 12.305c0-6.63-5.373-12.008-12-12.008z"
+							/>
+						</svg>
+						GitHub
+					</button>
+					<button
+						onclick="window.location.href='https://www.linkedin.com/in/davidbong05/'"
+						class="flex rounded-md dark:bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+					>
+						<svg
+							viewBox="0 0 16 16"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="currentColor"
+							class="h-5 w-5 mt-0.5 me-2"
 						>
-							<svg
-								viewBox="0 0 16 16"
-								xmlns="http://www.w3.org/2000/svg"
-								fill="currentColor"
-								class="h-5 w-5 mt-0.5 me-2"
-							>
-								<path
-									d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"
-								/>
-							</svg>
-
-							LinkedIn
-						</button>
-					</div>
+							<path
+								d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"
+							/>
+						</svg>
+						LinkedIn
+					</button>
 				</div>
 			</div>
 		</div>
 
 		<div class="mt-10 md:w-full sm:mx-auto sm:w-full sm:max-w-sm">
 			<p class="mb-4 text-slate-900 dark:text-white">
-				Get my resume and CV from the faucet below:
+				Get my resume and CV from the faucet below: 
 			</p>
 			<form class="space-y-6" action="#">
 				<div class="md:flex md:justify-center w-full">
@@ -89,6 +95,7 @@
 						Send
 					</button>
 				</div>
+				<div class="mt-2 text-slate-900 dark:text-white text-xs">* It will be send to your inbox instantly</div>
 			</form>
 		</div>
 		<div
